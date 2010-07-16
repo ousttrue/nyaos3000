0d052869a5e084fbaa0022265605f365aa3cdba9
HAYAMA_Kaoru <iyahaya@nifty.com>
2010-07-17 01:11:37 +0900

    * パッケージ名を nyaos3k-*-xxx.zip から nyaos-*-xxx.zip となるよう生成スクリプトを変更

--- mkpack.cmd
+++ mkpack.cmd
@@ -9,9 +9,9 @@ if not '%1' == '' goto main
     goto exit
 :main
 if not '%OS%' == 'Windows_NT' goto OS2
-    zip nyaos3k-%1-win.zip nyaos.exe _nya nyaos*.txt
-    hg archive -t zip nyaos3k-%1-src.zip
+    zip nyaos-%1-win.zip nyaos.exe _nya nyaos*.txt
+    hg archive -t zip nyaos-%1-src.zip
     goto exit
 :OS2
-    zip nyaos3k-%1-os2.zip nyaos.exe _nya nyaos*.txt
+    zip nyaos-%1-os2.zip nyaos.exe _nya nyaos*.txt
 :exit
